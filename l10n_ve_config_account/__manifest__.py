7b767ebff829ea70dc343cd6428fb13ea3b9782b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2024-10-30 09:42:25 -0400

    [UPD] Updated the 'name' value from the __manifest__.py file at the l10n_ve_config_account module

--- l10n_ve_config_account/__manifest__.py
+++ l10n_ve_config_account/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Omegasoft C.A Configuración contable",
+    "name": "Venezuela - Configuración contable",
     "version": "16.0.0.0.1",
     "category": "Accounting/Localizations/Account Charts",
     "author": "Omegasoft C.A., Odoo Community Association (OCA)",
